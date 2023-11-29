0d268b03e01fd3ccea7b2ad0d91448e974445c0f
MikeNsiah10 <106228933+MikeNsiah10@users.noreply.github.com>
2023-11-29 21:58:34 +0100

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -8,16 +8,13 @@ app = Flask(__name__)
 
 #directory where whoosh index is located
 index_dir = 'indexdir'
+index = open_dir(index_dir)
 
 @app.route('/')
 def home():
      # Render the HTML template for the search form
     return render_template('search_form.html')
 
-#handle internal server error
-@app.errorhandler(500)
-def internal_error(exception):
-   return "<pre>"+traceback.format_exc()+"</pre>"
 
 @app.route('/search')
 def search():
@@ -27,7 +24,7 @@ def search():
     if not query:
         return 'No search query provided.'
     #open whoosh index directory
-    index = open_dir(index_dir)
+    
     #perform search and return search results
     with index.searcher() as searcher:
         query_parser = QueryParser("content", index.schema)
@@ -35,6 +32,12 @@ def search():
         results = searcher.search(query_obj)
 
         return render_template('search_results.html', query=query, results=results)
+
+
+#handle internal server error
+@app.errorhandler(500)
+def internal_error(exception):
+   return "<pre>"+traceback.format_exc()+"</pre>"
 
 #run the flask app in debug mode
 if __name__ == '__main__':
