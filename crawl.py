82803c5af8e7faf922228f457393d627606598cf
MikeNsiah10 <106228933+MikeNsiah10@users.noreply.github.com>
2023-11-29 23:53:22 +0100

    Update crawl.py

--- crawl.py
+++ crawl.py
@@ -1,86 +1,90 @@
 import os
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin, urlparse
+from urllib.parse import urljoin
 from whoosh.index import create_in, open_dir
 from whoosh.fields import Schema, TEXT, ID
 from whoosh.qparser import QueryParser
 
-#defined class to parse HTML pages
+# Defined class to parse HTML pages
 class Crawler():
     def __init__(self, urls):
-        #initialise start urls and keep track of visited urls
+        # Initialise start urls and keep track of visited urls
         self.visited_urls = []
         self.url_to_visit = urls
 
     def crawl(self):
-        #perform a web crawl
+        # Perform a web crawl
         while self.url_to_visit:
-            #pop the start url and add to visited urls
+            # Pop the start url and add to visited urls
             url = self.url_to_visit.pop(0)
             self.visited_urls.append(url)
-            #extract HTML content and handle exceptions
+            # Extract HTML content and handle exceptions
             try:
-             html = requests.get(url,timeout=3).text
-            except:
-             print(f'unable to fetch html content from {url}')
-            continue
-        soup = BeautifulSoup(html, 'html.parser')
-            #extract the links 
-        for link in soup.find_all('a'):
+                html = requests.get(url, timeout=3).text
+            except Exception as e:
+                print(f"Unable to fetch HTML content from {url}: {e}")
+                continue
+
+            soup = BeautifulSoup(html, 'html.parser')
+            # Extract the links
+            for link in soup.find_all('a'):
                 link_path = link.get('href')
                 if link_path and link_path.startswith('/'):
                     link_path = urljoin(url, link_path)
-                    #add urls to the list if not visited
+                    # Add URLs to the list if not visited
                     if link_path not in self.visited_urls and link_path not in self.url_to_visit:
                         self.url_to_visit.append(link_path)
-    # create the index with title,content,url
+
+    # Create the index with title, content, url
     def create_index(self):
         schema = Schema(title=TEXT(stored=True), content=TEXT(stored=True), path=ID(stored=True))
         index_dir = "indexdir"
-        #create directory if path does not exist
+        # Create directory if path does not exist
         if not os.path.exists(index_dir):
             os.makedirs(index_dir)
-        
+
         index = create_in(index_dir, schema)
-        
+
         writer = index.writer()
-        
+
         for url in self.visited_urls:
-             #extract HTML content and handle exceptions
+            # Extract HTML content and handle exceptions
             try:
-              html = requests.get(url,timeout=3).text
-            except:
-              print(f'unable to fetch html content from {url}')
-              continue
+                html = requests.get(url, timeout=3).text
+            except Exception as e:
+                print(f"Unable to fetch HTML content from {url}: {e}")
+                continue
+
             soup = BeautifulSoup(html, 'html.parser')
-            # get the title of the page
+            # Get the title of the page
             title_tag = soup.find('title')
-            title_text = title_tag.text if title_tag else "no title was found"
-            # get content_text
+            title_text = title_tag.text if title_tag else "No title was found"
+            # Get content_text
             content_text = soup.get_text()
-            #add document to search index
+            # Add document to search index
             writer.add_document(title=title_text, content=content_text, path=url)
+
         writer.commit()
 
     def search(self, words):
-        #perform search using the search index
+        # Perform search using the search index
         index_dir = "indexdir"
-        index= open_dir(index_dir)
-        
+        index = open_dir(index_dir)
+
         with index.searcher() as searcher:
             query = QueryParser("content", index.schema).parse(" ".join(words))
             results = searcher.search(query)
-            # return results of search with title,url,teaser_text
+            # Return results of search with title, url, teaser_text
             for result in results:
                 print("Title:", result['title'])
                 print("URL:", result['path'])
                 print("Teaser_Text:", result.highlights("content"))
                 print()
 
-# testing code
-#create an instance of the crawler class
+# Testing code
+# Create an instance of the crawler class
 crawler = Crawler(urls=['https://vm009.rz.uos.de/crawl/index.html'])
 crawler.crawl()
 crawler.create_index()
-#crawler.search(["platypus","welcome"])
+crawler.search(["platypus", "welcome"])
