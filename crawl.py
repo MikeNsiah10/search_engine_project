aa46951f8765f57d9863cdbec574dc340ddd3c82
MikeNsiah10 <106228933+MikeNsiah10@users.noreply.github.com>
2023-12-02 16:07:52 +0100

    Updated crawl.py script

--- crawl.py
+++ crawl.py
@@ -1,96 +1,78 @@
-import os
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin
-from whoosh.index import create_in, open_dir
-from whoosh.fields import Schema, TEXT, ID
+from whoosh.index import create_in
+from whoosh.fields import *
 from whoosh.qparser import QueryParser
+from urllib.parse import urljoin, urlparse
 
-# Defined class to parse HTML pages
-class Crawler():
-    def __init__(self, urls):
-        # Initialise start urls and keep track of visited urls
-        self.visited_urls = []
-        self.url_to_visit = urls
+class Crawler:
+    def __init__(self, starting_url):
+        # Initialize  crawler with astarting URL
+        self.starting_url = starting_url
+        # Stack for URLs to be crawled
+        self.stack = [starting_url]
+        # keep track of visited URLs
+        self.visited = set()
+
+        # Define the schema for the Whoosh index
+        self.schema = Schema(url=TEXT(stored=True), title=TEXT(stored=True), content=TEXT(stored=True))
+        self.index_dir = "indexdir"
+        self.index = create_in(self.index_dir, self.schema)
 
     def crawl(self):
-        # Perform a web crawl
-        while self.url_to_visit:
-            # Pop the start url and add to visited urls
-            url = self.url_to_visit.pop(0)
-            self.visited_urls.append(url)
-            # Extract HTML content and handle exceptions
-            try:
-                html = requests.get(url, timeout=3).text
-            except Exception as e:
-                print(f"Unable to fetch HTML content from {url}: {e}")
-                continue
+        # Crawl the web 
+        while self.stack:
+            # Pop  URL from the stack
+            url = self.stack.pop(0)
+            if url not in self.visited:
+                try:
+                    # Send an HTTP request to the URL
+                    response = requests.get(url, timeout=5)
+                    print(f"Current URL: {url}")
 
-            soup = BeautifulSoup(html, 'html.parser')
-            # Extract the links
-            for link in soup.find_all('a'):
-                link_path = link.get('href')
-                if link_path and link_path.startswith('/'):
-                    link_path = urljoin(url, link_path)
-                    # Add URLs to the list if not visited
-                    if link_path not in self.visited_urls and link_path not in self.url_to_visit:
-                        self.url_to_visit.append(link_path)
+                    if response.status_code == 200:
+                        # Parse the HTML content of the page
+                        soup = BeautifulSoup(response.content, 'html.parser')
+                        # Extract title text or set a default if not found
+                        title_text = soup.find('title').text if soup.title else "No title was found"
+                        # Extract content without HTML tags
+                        content = ' '.join(soup.stripped_strings)
 
-    # Create the index with title, content, url, and definition
-    def create_index(self):
-        schema = Schema(title=TEXT(stored=True), content=TEXT(stored=True), path=ID(stored=True), definition=TEXT(stored=True))
-        index_dir = "indexdir"
-        # Create index_directory if index_directory does not exist
-        if not os.path.exists(index_dir):
-            os.makedirs(index_dir)
+                        # Add the document to the Whoosh index
+                        with self.index.writer() as writer:
+                            writer.add_document(url=url, title=title_text, content=content)
 
-        index = create_in(index_dir, schema)
+                        # Mark the URL as visited
+                        self.visited.add(url)
 
-        writer = index.writer()
+                        # Extract and add new URLs from the page to the stack
+                        for link in soup.find_all('a', href=True):
+                            absolute_url = urljoin(url, link['href'])
+                            if urlparse(absolute_url).netloc == urlparse(self.starting_url).netloc:
+                                if absolute_url not in self.visited and absolute_url not in self.stack:
+                                    self.stack.append(absolute_url)
 
-        for url in self.visited_urls:
-            # Extract HTML content and handle exceptions
-            try:
-                html = requests.get(url, timeout=3).text
-            except Exception as e:
-                print(f"Unable to fetch HTML content from {url}: {e}")
-                continue
+                except Exception as e:
+                    # Handle exceptions during the crawling process
+                    print(f"Error while processing {url}: {e}")
 
-            soup = BeautifulSoup(html, 'html.parser')
-            # Get the title of the page
-            title_tag = soup.find('title')
-            title_text = title_tag.text if title_tag else "No title was found"
-            # Get content_text and remove HTML tags
-            content_text = ' '.join(soup.stripped_strings)
-            # Add document to search index
-            writer.add_document(title=title_text, content=content_text, path=url)
+            # Print status information
+            print(f"Stack of websites left: {self.stack}")
+            print(f"Crawled websites: {self.visited}")
 
-        writer.commit()
-
-    def search(self, words):
-    # Perform search using the search index
-     index_dir = "indexdir"
-     index = open_dir(index_dir)
-
-     with index.searcher() as searcher:
-        query = QueryParser("content", index.schema).parse(" ".join(words))
-        results = searcher.search(query)
-        # Return results of search with title, url, teaser_text, and definition
-        for result in results:
-            # Extract title, URL, and teaser_text without HTML tags
-            title_text = result['title']
-            url = result['path']
-            teaser_text = ' '.join(BeautifulSoup(result.highlights("content"), 'html.parser').stripped_strings)
-
-            # Print the extracted information
-            print("Title:", title_text)
-            print("URL:", url)
-            print("Teaser_Text:", teaser_text)
-            print()
+    def search(self, query):
+        # Search the Whoosh index given a query
+        with self.index.searcher() as searcher:
+            # Parse the query and search for matching documents
+            query = QueryParser("content", self.index.schema).parse(query)
+            # return a list of results with URL, title, and content
+            results = [{'url': result['url'], 'title': result['title'], 'content': result['content']}
+                       for result in searcher.search(query)]
+            return results
 
 # Testing code
-# Create an instance of the crawler class
-crawler = Crawler(urls=['https://vm009.rz.uos.de/crawl/index.html'])
+#create an instance of the class
+crawler = Crawler(starting_url='https://vm009.rz.uos.de/crawl/')
 crawler.crawl()
-crawler.create_index()
-crawler.search(["platypus", "welcome"])
+search_word = "plapytus"  # 
+crawler.search(search_word)
